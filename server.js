fda5613bad1d382b6ad8c253a4f8883e97699453
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-06-06 10:39:49 -0400

    add dotenv to manage keys, currently not working

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express'),
       path = require('path'),
       bodyParser = require('body-parser'),
@@ -18,59 +19,57 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('./summoners', summonerRoutes);
 
-app.get('/search', (req, res) => {
-  let data = {};
-  let finalData = {};
-  let api_key = 'RGAPI-36d29bf0-d3a1-4952-86c8-9c20ccf6ff1c';
-  let name = 'malifaux';
-  let accountId = '';
-  let URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + name + '?api_key=' + api_key;
-  let URL2 = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/' + accountId + '?api_key=' + api_key;
-
-  async.waterfall([
-
-    (callback) => {
-      request(URL, (err, response, body) => {
-        if (!err && response.statusCode === 200) {
-          var json = JSON.parse(body);
-          data = json;
-
-
-          callback(null, data);
-        } else {
-          console.log(err);
-        }
-      })
-    },
-
-    (obj, callback) => {
-      accountId = obj.accountId;
-      URL2 = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/' + accountId + '?api_key=' + api_key;
-      request(URL2, (err, response, body) => {
-      if (!err && response.statusCode === 200) {
-        var json = JSON.parse(body);
-        extend(finalData, json, data);
-        console.log(finalData);
-        callback(null, data);
-      } else {
-        console.log(err);
-      }
-    })
-  }
-  ],
-(err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  res.send(finalData);
-})
-});
-
-// app.get('/', (req, res) => {
-//   res.send("YOYOYO");
+// app.get('/search', (req, res) => {
+//   let data = {};
+//   let finalData = {};
+//   let api_key = process.env.API_KEY;
+//   let name = 'malifaux';
+//   let accountId = '';
+//   let URL = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + name + '?api_key=' + api_key;
+//   let URL2 = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/' + accountId + '?api_key=' + api_key;
+//
+//   async.waterfall([
+//
+//     (callback) => {
+//       request(URL, (err, response, body) => {
+//         if (!err && response.statusCode === 200) {
+//           var json = JSON.parse(body);
+//           data = json;
+//           callback(null, data);
+//         } else {
+//           console.log(err);
+//         }
+//       })
+//     },
+//
+//     (obj, callback) => {
+//       accountId = obj.accountId;
+//       URL2 = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/' + accountId + '?api_key=' + api_key;
+//       request(URL2, (err, response, body) => {
+//       if (!err && response.statusCode === 200) {
+//         var json = JSON.parse(body);
+//         extend(finalData, json, data);
+//         console.log(finalData);
+//         callback(null, data);
+//       } else {
+//         console.log(err);
+//       }
+//     })
+//   }
+//   ],
+// (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//
+//   res.send(finalData);
+// })
 // });
+
+app.get('/', (req, res) => {
+  res.send(process.env.hello);
+});
 
 const port = process.env.PORT || 4000;
 
